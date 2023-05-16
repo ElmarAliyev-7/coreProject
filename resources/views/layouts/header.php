f22c3e324ff1ac9813b26ede4d8e329207ddb302
ElmarAliyev-7 <liyevelmar02@gmail.com>
2023-05-16 17:17:43 +0400

    Front Template

--- resources/views/layouts/header.php
+++ resources/views/layouts/header.php
@@ -3,36 +3,58 @@ require_once 'System/helpers.php';
 const baseUrl = 'http://localhost:8080';
 ?>
 <!doctype html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Elmar Aliyev</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>The7 Free HTML5 Responsive Template | Template Stock</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?=baseUrl;?>/public/front/css/reset.css">
+    <link rel="stylesheet" href="<?=baseUrl;?>/public/front/css/simplegrid.css">
+    <link rel="stylesheet" href="<?=baseUrl;?>/public/front/css/icomoon.css">
+    <link rel="stylesheet" href="<?=baseUrl;?>/public/front/css/lightcase.css">
+    <link rel="stylesheet" href="<?=baseUrl;?>/public/front/js/owl-carousel/owl.carousel.css" />
+    <link rel="stylesheet" href="<?=baseUrl;?>/public/front/js/owl-carousel/owl.theme.css" />
+    <link rel="stylesheet" href="<?=baseUrl;?>/public/front/js/owl-carousel/owl.transitions.css" />
+    <link rel="stylesheet" href="<?=baseUrl;?>/public/front/style.css">
+
+    <!-- Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,900' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
-<body>
-<!-- Start navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="<?=baseUrl;?>">Elmar Aliyev</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item <?=is_current('/');?>">
-                <a class="nav-link" href="<?=baseUrl;?>">Home</a>
-            </li>
-            <li class="nav-item <?=is_current('/blogs');?>">
-                <a class="nav-link" href="<?=baseUrl;?>/blogs">Blogs</a>
-            </li>
-            <li class="nav-item <?=is_current('/blogs/create');?>">
-                <a class="nav-link" href="<?=baseUrl;?>/blogs/create">Create Blog</a>
-            </li>
-        </ul>
+<body id="home">
+<!-- Header -->
+<header id="top-header" class="header-home">
+    <div class="grid">
+        <div class="col-1-1">
+            <div class="content">
+                <div class="logo-wrap">
+                    <a href="#0" class="logo">The7</a>
+                </div>
+                <nav class="navigation">
+                    <input type="checkbox" id="nav-button">
+                    <label for="nav-button" onclick></label>
+                    <ul class="nav-container">
+                        <li><a href="#home" class="current">Home</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#work">Work</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                        <li><a href="#pricing">Pricing</a></li>
+                        <li><a href="#team">Team</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </div>
-</nav>
-<!-- End of navbar -->
+</header>
+<!-- End Header -->
--- resources/views/widgets/blogs.php
+++ resources/views/widgets/blogs.php
@@ -1,15 +1,27 @@
-<div class="container">
-    <div class="row">
-        <?php foreach ($blogs as $blog): ?>
-            <div class="card col-6" style="width: 18rem;">
-                <img class="card-img-top" src="<?=baseUrl . "/" . $blog['cover'];?>"
-                     alt="Card image cap" height="300px">
-                <div class="card-body">
-                    <h5 class="card-title"><?=$blog['title'];?></h5>
-                    <p class="card-text"><?=str_limit($blog['description'], 200, '...');?></p>
-                    <a href="<?=baseUrl;?>/blogs/show/<?=$blog['id'];?>" class="btn btn-primary">Show More</a>
-                </div>
+<div class="wrap blog-grid grey" id="blog">
+    <div class="grid grid-pad">
+        <div class="content" >
+            <h2>Our Blog</h2>
+            <?php foreach ($blogs as $blog): ?>
+                <div class="col-1-2" >
+                <article class="post-wrap">
+                    <div class="post-img">
+                        <a href="#0"><img src="<?=baseUrl . "/" . $blog['cover'];?>" alt=""></a>
+                    </div>
+                    <div class="post">
+                        <h2 class="entry-title"><a href="<?=baseUrl;?>/blogs/show/<?=$blog['id'];?>">
+                                <?=$blog['title'];?>
+                            </a></h2>
+                        <div class="post-meta">
+                            <a href="<?=baseUrl;?>/blogs/show/<?=$blog['id'];?>">2 Februar, 2014</a> <span class="mid-sep">·</span> <a href="#0">Photography</a>
+                        </div>
+                        <p><?=str_limit($blog['description'], 200, '...');?></p>
+                        <a class="btn read-more" href="<?=baseUrl;?>/blogs/show/<?=$blog['id'];?>">Read More</a>
+                    </div>
+                </article>
             </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+            <div class="col-1-1"><a class="btn" href="<?=baseUrl;?>blogs>">View All</a></div>
+        </div>
     </div>
 </div>
