bd965aaa26d06c15a004d391dd0a39a25da36064
ElmarAliyev-7 <liyevelmar02@gmail.com>
2023-05-18 10:30:13 +0400

    fixed

--- resources/views/blogs/index.php
+++ resources/views/blogs/index.php
@@ -11,35 +11,34 @@ if(isset($_POST['delete'])) :
     header("Refresh:1");
 endif;
 ?>
-<h1 class="text-center">All Blogs</h1>
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
+<div class="table-responsive">
+    <table class="table table-light">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Cover</th>
+            <th scope="col">Status</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach($blogs as $blog) : ?>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">Title</th>
-                <th scope="col">Description</th>
-                <th scope="col">Cover</th>
-                <th scope="col">Status</th>
-                <th scope="col">Actions</th>
+                <th scope="row"><?=$blog['id'];?></th>
+                <td><?=$blog['title'];?></td>
+                <td><?=str_limit($blog['description'], 20);?></td>
+                <td><img src="<?=baseUrl . "/" . $blog['cover'];?>" alt="Cover image" height="100px" width="100px"></td>
+                <td><?=$blog['status'];?></td>
+                <td>
+                    <form action="" method="post">
+                        <button name="delete" class="btn btn-danger" type="submit" value="<?=$blog['id'];?>">Delete</button>
+                    </form>
+                </td>
             </tr>
-            </thead>
-            <tbody>
-            <?php foreach($blogs as $blog) : ?>
-                <tr>
-                    <th scope="row"><?=$blog['id'];?></th>
-                    <td><?=$blog['title'];?></td>
-                    <td><?=str_limit($blog['description'], 20);?></td>
-                    <td><img src="<?=baseUrl . "/" . $blog['cover'];?>" alt="Cover image" height="100px" width="100px"></td>
-                    <td><?=$blog['status'];?></td>
-                    <td>
-                        <form action="" method="post">
-                            <button name="delete" class="btn btn-danger" type="submit" value="<?=$blog['id'];?>">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            <?php endforeach;?>
-            </tbody>
-        </table>
-    </div>
+        <?php endforeach;?>
+        </tbody>
+    </table>
+</div>
 <?php include_once 'resources/views/layouts/footer.php'; ?>
